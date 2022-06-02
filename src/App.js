fa3b437b1673cdca31a9d45ebe1a1f84f7354e86
SATHYA SIVAM <98577861+sathyasivamloganathan@users.noreply.github.com>
2022-06-02 10:34:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 import './App.css';
 //import Login from './Login';
 //import Signin from './Signin';
-import FrogetPassword from './ForgetPassword';
-import Events from './Events';
+//import FrogetPassword from './ForgetPassword';
+import Events from './EventsDescription';
 function App() {
   return (
     <div className="UNITE">
       {/* <Login />   */}
       {/*<Signin /> */} 
 {/* < FrogetPassword /> *}
-<Events/>
+<EventsDescription/>
     </div>
   );
 }
